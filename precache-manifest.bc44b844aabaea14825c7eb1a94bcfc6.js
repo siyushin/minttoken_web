3baedac1e62755bbbdc41eaec8e45312944525ae
Yushin Si <siyushin@hotmail.com>
2020-09-28 16:30:59 +0800

    make it better

---
+++ precache-manifest.bc44b844aabaea14825c7eb1a94bcfc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7aa14cb4b0eba11136c5924179a56bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe46c3d40c47c447c0c",
+    "url": "/static/css/main.71008f64.chunk.css"
+  },
+  {
+    "revision": "458c33989d2dff7001cc",
+    "url": "/static/js/2.fd59a116.chunk.js"
+  },
+  {
+    "revision": "bc6f7b0e5469753ca344737161714202",
+    "url": "/static/js/2.fd59a116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe46c3d40c47c447c0c",
+    "url": "/static/js/main.1bd5d211.chunk.js"
+  },
+  {
+    "revision": "7707c0b92798e828a705",
+    "url": "/static/js/runtime-main.174b5fcb.js"
+  }
+]);
